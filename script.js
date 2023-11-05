2b328d6e81cd88c7bdf8256574a34bd0adc34336
Lancer385 <102915355+Lancer385@users.noreply.github.com>
2023-11-05 15:52:42 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const password = document.querySelector("#password")
+  , confirm_password = document.querySelector("#cPassword");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
