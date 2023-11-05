a9575f09a9cbde95a9f5aedbc815391cf4f7489a
Lancer385 <102915355+Lancer385@users.noreply.github.com>
2023-11-05 20:05:00 +0200

    update

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ function validatePassword(){
   }
 }
 
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+password.addEventListener('change',validatePassword);
+confirm_password.addEventListener('keyup',validatePassword);
